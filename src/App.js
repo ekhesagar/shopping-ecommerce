5424665ecb6ca610e9ebc688c5d0d2b9dceb4760
Sagar Ekhe <sagar@Sagars-Air.attlocal.net>
2021-01-04 16:10:18 -0800

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
-import Homepage from './homepage.component'
+import Homepage from './pages/homepage/homepage.component.jsx'
 function App() {
+
+  const HatsPage = (props)=>{
+    console.log(props)
+    return (
+      <div>
+        Hats page
+      </div>
+    )
+  }
+
   return (
     <div>
-     <Homepage />
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
     </div>
   );
 }
