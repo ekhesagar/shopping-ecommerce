75b3e003172e72239812880ce3221575f04d8ed5
Sagar Ekhe <sagar@Sagars-MacBook-Air.local>
2021-01-14 19:00:46 -0800

    added new shop data, updated cart utils in cart reducer to grp duplicate items in cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
+import CustomButton from '../custom-button/custom-button.component'
+
+import { addItem } from '../../redux/cart/cart.actions'
 
 import './collection-item.styles.scss'
 
-const CollectionItem = ({ name, imageUrl, price }) => (
-    <div className='collection-item'>
+const CollectionItem = ({ item, addItem }) => {
+    const { name, imageUrl, price } = item
+
+   return ( <div className='collection-item'>
         <div 
         className='image'
         style={{
@@ -14,7 +21,13 @@ const CollectionItem = ({ name, imageUrl, price }) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton onClick={()=>{addItem(item)}} inverted >ADD TO CART</CustomButton>
     </div>
-)
+   )
+}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
